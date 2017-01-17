3d3603cfbd0edaa78225ecaa0627487ced90ed9b
Robert Whiteley <fd8955@wayne.edu>
2017-01-16 20:25:36 -0500

    Never done this before
    Adding the actual file.. lol

---
+++ todo.php
@@ -0,0 +1,42 @@
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>To do project fd8955 4996 WSU</title>
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+
+</head>
+
+<body>
+
+
+
+<div class="list">
+		<h1> To Do FD8955. </h1>
+		
+		<ul>
+		
+		<li> 
+		<span> Oh snap </span>
+		<a href="#"> Mark as done</a>
+		</li>
+		
+		<li> 
+		<span> I'm learning how to use SQl </span>
+		</li>
+		
+</ul>
+
+<form class="add.php" method="post">
+
+	<input type="text" name="" placeholder=" Type a new item here." autocomplete="off" required>
+	<input type= "Submit" value="Add" class= "submit">
+
+
+
+</body>
+</html>
+
